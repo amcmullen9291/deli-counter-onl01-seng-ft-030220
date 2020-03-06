09245e43b0246e7eada0d176fb2e69dbea83aaf7
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-06 20:25:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,11 @@ def line (array)
 if array.length== 0 
   puts "The line is currently empty."
 else
-  current_line = "The line is currently: "
+  current_line = "The line is currently:"
   array.each.with_index(1) do |name,index|
-    current_line<< "#{index}.  #{name}"
+    current_line<< " #{index}.  #{name}"
   end
+  puts current_line
 end
 end
 
