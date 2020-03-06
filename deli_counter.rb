70638e212fdbb82b761f7ac50be927560b540095
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-06 20:25:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,12 +10,3 @@ else
 end
 end
 
-def take_a_number(array, name)
-  number= array.length+1 #your position in line
-  array.each {|name| puts "Welcome, #{name}. You are number #{number} in line"} 
-end
-
-def now_serving
-  array.delete_at(0)
-    puts "There is nobody waiting to be served!"
-end
