519c01b780cc1ced3632c08bc689750002f7ca3b
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-06 20:08:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line (array)
+  array.each|name| 
+  if array.size== 0
+  array.map.with_index { |w, i| "#{i}:#{w}"
+end
+
+def take_a_number(array, name)
+  number= array.length+1 #your position in line
+  array.each {|name| puts "Welcome, #{name}. You are number #{number} in line"} 
+end
+
+def now_serving
+  array.delete_at(0)
+    puts "There is nobody waiting to be served!"
+end
